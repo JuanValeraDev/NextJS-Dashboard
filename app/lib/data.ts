eca034cdbfd92c75f9eac0f71c5e89cfb88e9aa0
JuanValeraDev <juanvaleradev@gmail.com>
2024-08-06 18:38:08 +0200

    Finished chapter 10

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -29,7 +29,7 @@ export async function fetchRevenue() {
 export async function fetchLatestInvoices() {
 
     console.log('Fetching latest invoices...');
-    await new Promise((resolve) => setTimeout(resolve, Math.floor(Math.random() * (5000 - 1000 + 1)) + 1000));
+    await new Promise((resolve) => setTimeout(resolve, Math.floor(Math.random() * (4000 - 1000 + 1)) + 1000));
 
     try {
         const data = await sql<LatestInvoiceRaw>`
